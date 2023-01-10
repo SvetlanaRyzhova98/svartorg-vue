dddbec196294d34faae408b217b6231386316532
sweta__7_ <sweta300498@yandex.ru>
2023-01-10 18:06:58 +0600

    nobr

--- components/MainHero/index.vue
+++ components/MainHero/index.vue
@@ -3,7 +3,7 @@
     <div class="hiro__decor"></div>
 
     <div class="hiro__box">
-      <title class="hiro__title">Сварочное оборудование<br /></title>
+      <title class="hiro__title">Сварочное оборудование </title>
       <div class="hiro__desc">
         <p>
           Оптово-розничная торговля: cварочное оборудование,
