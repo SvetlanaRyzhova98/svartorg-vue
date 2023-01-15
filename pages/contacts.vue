f91e47381836bd6bb09a200056fff5414cb13d83
sweta__7_ <sweta300498@yandex.ru>
2023-01-15 14:38:11 +0600

    location

--- pages/contacts.vue
+++ pages/contacts.vue
@@ -1,5 +1,5 @@
 <template>
-      <Head>
+  <Head>
     <Title>Контакты</Title>
     <Meta
       name="description"
@@ -9,11 +9,31 @@
       name="keywords"
       content="Уральская 136, Краснодар, Тимашевск, Пролетарская 106, Новороссийск"
     />
-   
-    <Link rel="icon" href="https://www.galaktika-svarki.ru/favicon.ico" type="image/x-icon" />
-    
+
+    <Link
+      rel="icon"
+      href="https://www.galaktika-svarki.ru/favicon.ico"
+      type="image/x-icon"
+    />
   </Head>
-    <div>
-        contacts
+  <SecondaryHero>Контакты</SecondaryHero>
+
+  <section class="wrapper">
+    <BreadCrumbs
+      :path="[{ caption: 'Контакты', href: '/contacts' }]"
+    />
+    <div class="contacts_box">
+      <img src="" alt="">
+
     </div>
+  </section>
+    
 </template>
+
+<style>
+
+.contacts_box{
+  margin: 30px 0;
+  border-top: 2px solid gray;
+}
+</style>
