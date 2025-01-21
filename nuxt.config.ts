b95ba089afd2fbeac24aebd9b4bf3efb8336477c
Svetlana98 <sveti.300498@gmail.com>
2025-01-21 22:28:34 +0500

    new

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,36 @@ export default defineNuxtConfig({
     app: {
       head: {
         link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+        script: [
+            {
+                hid: 'yandex-metrika',
+                children: `(function (m, e, t, r, i, k, a) {
+        m[i] =
+          m[i] ||
+          function () {
+            (m[i].a = m[i].a || []).push(arguments);
+          };
+        m[i].l = 1 * new Date();
+        for (var j = 0; j < document.scripts.length; j++) {
+          if (document.scripts[j].src === r) {
+            return;
+          }
+        }
+        (k = e.createElement(t)),
+          (a = e.getElementsByTagName(t)[0]),
+          (k.async = 1),
+          (k.src = r),
+          a.parentNode.insertBefore(k, a);
+      })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+      ym(99172841, "init", {
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true,
+      });`,
+      type: 'text/javascript',
+            }
+        ]
       },
     },
     
