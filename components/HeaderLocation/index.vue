1e12e192f23048e801f1c12f197e7eb0232f5f5f
Svetlana98 <sveti.300498@gmail.com>
2025-05-07 14:46:55 +0500

    фикс "добавление,а не замена номера"

--- components/HeaderLocation/index.vue
+++ components/HeaderLocation/index.vue
@@ -2,25 +2,34 @@
   <div class="header__contact">
     <div class="header__link header__link_cont">
       <ClientOnly><font-awesome-icon icon="fa-solid fa-location-dot" /></ClientOnly>
-      {{ Locations[location.location]?.address }}
+      {{ currentAddress }}
     </div>
 
     <a
-      :href="'tel:' + Locations[location.location]?.phone"
+      :href="'tel:' + currentPhone"
       class="header__link header__link_cont"
     >
       <ClientOnly><font-awesome-icon icon="fa-solid fa-phone" /></ClientOnly>
-      {{ Locations[location.location]?.phone }}
+      {{ currentPhone }}
     </a>
-   
   </div>
 </template>
 
 <script setup>
 import { useLocationStore } from "../../store/location";
 import { Locations } from "../../consts/location";
+import { computed } from 'vue';
 
-const location = useLocationStore();
+const locationStore = useLocationStore();
+
+// Вычисляемые свойства для адреса и телефона
+const currentAddress = computed(() => {
+  return Locations[locationStore.location]?.address || '';
+});
+
+const currentPhone = computed(() => {
+  return Locations[locationStore.location]?.phone || '';
+});
 </script>
 
 <style>
